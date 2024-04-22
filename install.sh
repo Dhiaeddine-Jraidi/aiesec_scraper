eee13f4796151f8540f8fcabf4a12f8f2930b69c
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-04-22 02:06:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,11 @@ unzip -o chromedriver_linux64.zip
 sudo mv -f chromedriver /usr/local/bin/
 sudo rm -f chromedriver_linux64.zip google-chrome-stable_114.0.5735.90-1_amd64.deb
 echo "Chrome version 114.0.5735.90 and Chrome WebDriver version 114.0.5735.16 installation completed successfully!"
+
 git clone https://github.com/Dhiaeddine-Jraidi/aiesec_scraper.git
 sudo chmod a+rwx aiesec_scraper
 cd aiesec_scraper
-pip3 install -r requirements.txt
-(crontab -l 2>/dev/null; echo "0 0 * * * cd ~/aiesec_scraper && python3 main.py") | crontab -
+sudo pip3 install -r requirements.txt
+cd ~
+sudo rm -f LICENSE.chromedriver
+(crontab -l 2>/dev/null; echo "0 0 * * * cd ~/aiesec_scraper && sudo python3 main.py") | crontab -
