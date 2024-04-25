c876073996d518183ed1e7267f96c0ff7e9c9dd8
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-04-26 00:28:24 +0100

    ..

--- glob_functions.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BOT_TOKEN_key = "7099537302:AAEg_9hW3k6u1qKTwpH28McZD546xIWa9ik"
-
-
-def send_message(message):
-    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN_key}/sendMessage", data={'chat_id': '1545111998','text': message})
